62d21c32f1b01f93ffeff6479686b7f881aef287
Oleg998 <137883971+Oleg998@users.noreply.github.com>
2024-02-13 13:57:08 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,39 @@
 import { Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { lazy, Suspense } from 'react';
+import Loader from './Loader/Loader';
 
-import SharedLayout from './SharedLayout/SharedLayout';
-import MoviesPage from 'pages/MoviesPage/MoviesPage';
-import HomePage from 'pages/HomePage/HomePage';
-import SingleMoviesPage from 'pages/SingleMoviesPage/SingleMoviesPage';
+const SharedLayout = lazy(() => import('./SharedLayout/SharedLayout'));
+const MoviesPage = lazy(() => import('pages/MoviesPage/MoviesPage'));
+const HomePage = lazy(() => import('pages/HomePage/HomePage'));
+const SingleMoviesPage = lazy(() =>
+  import('pages/SingleMoviesPage/SingleMoviesPage')
+);
 
-import Coast from 'pages/SingleMoviesPageCoast/SingleMoviesPageCoast';
-import Reviews from 'pages/SingleMoviesPageReviews/SingleMoviesPageReviews';
+const Coast = lazy(() =>
+  import('pages/SingleMoviesPageCoast/SingleMoviesPageCoast')
+);
+const Reviews = lazy(() =>
+  import('pages/SingleMoviesPageReviews/SingleMoviesPageReviews')
+);
 export const App = () => {
   return (
     <div>
       <ToastContainer autoClose={5000} />
-      <Routes>
-        <Route path="/" element={<SharedLayout />}>
-          <Route index element={<HomePage />} />
-          <Route path="movies" element={<MoviesPage />} />
-          <Route path="*" element={<HomePage/>} />
-          <Route path="movies/:id" element={<SingleMoviesPage />}>
-            <Route path="coast" element={<Coast />} />
-            <Route path="reviews" element={<Reviews />} />
+      <Suspense fallback={Loader}>
+        <Routes>
+          <Route path="/" element={<SharedLayout />}>
+            <Route index element={<HomePage />} />
+            <Route path="movies" element={<MoviesPage />} />
+            <Route path="*" element={<HomePage />} />
+            <Route path="movies/:id" element={<SingleMoviesPage />}>
+              <Route path="coast" element={<Coast />} />
+              <Route path="reviews" element={<Reviews />} />
+            </Route>
           </Route>
-        </Route>
-      </Routes>
+        </Routes>
+      </Suspense>
     </div>
   );
 };
